c9ba3e8ec8c387ff97b53a24242b38f1e22f4256
Rémi Prévost <remi@exomel.com>
2011-04-04 13:50:08 -0400

    Add dirty hack to make it work on 1.9

--- lib/annyong.rb
+++ lib/annyong.rb
@@ -2,3 +2,11 @@ module Annyong
   VERSION = '0.1'
   autoload :Middleware, "annyong/middleware"
 end
+
+unless "".respond_to?(:each)
+  String.class_eval do
+    def each &block
+      self.lines &block
+    end
+  end
+end
